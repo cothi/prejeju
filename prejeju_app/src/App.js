96a5190b8120aa1d83151fa097e7f6781af5bfa9
kastro-team <jiungdev@gmail.com>
2022-08-30 18:50:43 +0900

    feat: add react router dom

--- prejeju_app/src/App.js
+++ prejeju_app/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Cafe from "./pages/Cafe"
 import DateSelect from "./pages/dateSelect"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <DateSelect />
+    <div>
+      heelo
     </div>
+
   );
 }
 export default App;
