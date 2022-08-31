15e487b2933a16cb497cd7e14d6e80d9341c2e76
kastro-team <jiungdev@gmail.com>
2022-08-31 10:58:18 +0900

    feat: kakaomap api import

--- prejeju_app/src/App.js
+++ prejeju_app/src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
-import Cafe from "./pages/Cafe"
-import DateSelect from "./pages/dateSelect"
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import logo from "./assets/img/logo.svg";
+import wave from "./assets/img/wave.svg";
+import surfingboard from "./assets/img/surfingboard.svg"
+import surfingboard2 from "./assets/img/surfingboard2.svg"
+import stone from "./assets/img/stone.svg"
+import tree from "./assets/img/tree.svg"
+import { Link } from "react-router-dom";
+
 
 function App() {
+
   return (
-    <div>
-      heelo
+    <div className="App">
+      <div>
+        <img src={logo} className="logo" alt="로고" />
+        <h1 className="mainTitleLogo">내 주머니</h1>
+      </div>
+
+      <Link to="/type">
+        <div className="button">
+          <button className="mainButton">여행 경비 추측하러 가기</button>
+        </div>
+      </Link>
+      <img src={wave} className="wave" alt="파도" />
+      <img src={surfingboard2} className="surfingboard2" alt="서핑보드2" />
+      <img src={surfingboard} className="surfingboard" alt="서핑보드" />
+      <img src={stone} className="stone" alt="돌하르방" />
+      <img src={tree} className="tree" alt="야자수" />
     </div>
 
   );
--- prejeju_app/src/pages/Cafe.js
+++ prejeju_app/src/pages/Cafe.js
@@ -1,9 +1,39 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { RenderAfterNavermapsLoaded, NaverMap } from 'react-naver-maps'
+import { Map, MapMarker } from 'react-kakao-maps-sdk'
+
+// 1f7afd5b956dfb17cf04da6a826eb37d
 
 function Cafe() {
+
+
   return (
     <div>
-      cafe
+      <div>
+        <div>
+          카페
+        </div>
+        <div>
+          Map
+        </div>
+      </div>
+      <div>
+        <div>
+          <Map
+            center={{ lat: 33.5563, lng: 126.79581 }}
+            style={{ width: "100%", height: "360px" }}
+          >
+            <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+              <div style={{ color: "#000" }}>Hello World!</div>
+            </MapMarker>
+            <MapMarker position={{ lat: 33.55635, lng: 126.795750 }}>
+              <div style={{ color: "#000" }}>Hello World!!</div>
+            </MapMarker>
+          </Map>
+
+        </div>
+        <div>map</div>
+      </div>
     </div>
   )
 }
